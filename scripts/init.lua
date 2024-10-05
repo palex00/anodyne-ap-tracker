13e14099da83b7432859a97d5488a8ab47366205
palex00 <32203971+palex00@users.noreply.github.com>
2024-10-05 21:48:33 +0200

    Add files via upload

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,25 +1,21 @@
+-- Idk even what this does
+local variant = Tracker.ActiveVariantUID
 
-            local variant = Tracker.ActiveVariantUID
-
+-- Items
 Tracker:AddItems("items/items.json")
 Tracker:AddItems("items/locationobjects.json")
 Tracker:AddItems("items/options.json")
-
+Tracker:AddItems("items/gates.json")
 
 -- Logic
 ScriptHost:LoadScript("scripts/logic/logic.lua")
 
 -- Maps
-if Tracker.ActiveVariantUID == "maps-u" then
-    Tracker:AddMaps("maps/maps-u.json")  
-else
-    Tracker:AddMaps("maps/maps.json")  
-end  
-
+Tracker:AddMaps("maps/maps.json")
 
 -- Layout
 Tracker:AddLayouts("layouts/broadcast.json")
-Tracker:AddLayouts("layouts/items.json")
+Tracker:AddLayouts("layouts/items_key.json")
 Tracker:AddLayouts("layouts/tabs.json")
 Tracker:AddLayouts("layouts/settings.json")
 Tracker:AddLayouts("layouts/tracker.json")
@@ -29,11 +25,11 @@ Tracker:AddLocations("locations/Regions.json")
 Tracker:AddLocations("locations/Anodyne.json")
 Tracker:AddLocations("locations/Submaps.json")
 Tracker:AddLocations("locations/PositionAuto.json")
+Tracker:AddLocations("locations/Dust.json")
 
 -- AutoTracking for Poptracker
-if PopVersion and PopVersion >= "0.18.0" then
     ScriptHost:LoadScript("scripts/autotracking.lua")
 
-end
-
+-- Watch Codes
 ScriptHost:AddWatchForCode("SwapableAreas", "*", swapareatoggle)
+ScriptHost:AddWatchForCode("KeymodeWatch", "opt_smallkey", toggle_keys)
